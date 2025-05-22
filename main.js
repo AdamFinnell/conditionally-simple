167c1640603970ab490d8ea42f7fa6adebaf330f
AdamFinnell <afinnell@perseverenow.org>
2025-05-22 11:55:35 -0500

    Cleaner Code

--- main.js
+++ main.js
@@ -35,11 +35,7 @@ function isItShort(str) {
 function howLongIsMyString(str) {
   if(str.length > 20) {
     return "That's a long string!"
-  }else if(str.length === 20){
-    return "That's a regular sized string!"
-  }else if(str.length > 10 && str.length < 20) {
-    return "That's a regular sized string!"
-  }else if(str.length === 10){
+  }else if(str.length >= 10){
     return "That's a regular sized string!"
   }else {
     return "That's a small string!"
